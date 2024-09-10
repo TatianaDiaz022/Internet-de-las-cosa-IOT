ec073d960202e9aaaf00c6330c7a2b3678e89457
Tatiana <ximenatatianadiazrosero@gmail.com>
2024-09-09 20:24:30 -0500

    iot

---
+++ scripts_python/math.py
@@ -0,0 +1,19 @@
+#Developer: Tatiana
+#Date: 26082024
+
+''' This is an Python example Script.
+    This program let ' u add two number
+    and show the anawer in your screen
+'''
+
+n1 = 10
+n2 = 30
+
+add = n1 +n2
+
+print(add)
+print("The aswer is:", add)
+print("The aswer is:" + str(add))
+print(F"The aswermis: {add}")
+print(F"The aswer is : {n1+n2}")
+print(F"The aswer is :", n1+n2)
